4bc005484d37b9ad1d068b4b33b491435b20c2cb
Olenka Hryk <olena.hryk@gmail.com>
2023-05-17 23:30:59 +0300

    Added conditions & task references

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,40 +1,34 @@
-// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+// ---------- 1 ------------
+// Messi Goals              https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+//
 
-function hero(bullets, dragons) {
-  if (bullets >= dragons * 2) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
-// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
 
-function hero(bullets, dragons) {
-  if (bullets >= dragons * 2) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// ---------- 2 ------------
+// Make negative            https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+//
 
-// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
 
-function hero(bullets, dragons) {
-  if (bullets >= dragons * 2) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
-// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+// ---------- 3 ------------
+// Game Move               https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+//
 
-function hero(bullets, dragons) {
-  if (bullets >= dragons * 2) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
+
+// ---------- 4 ------------
+// Personalized Message   https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+//
+
+
+
+
+// ---------- 5 ------------
+// Keep Hydrated          https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+//
+
+
+
+// ---------- 6 ------------
+// Opposites Attract      https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+//
