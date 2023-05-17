602b2095d778eae66ab04af0fe419c954e04de4a
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2023-05-17 21:17:16 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons) {
+  if (bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons) {
+  if (bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons) {
+  if (bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons) {
+  if (bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
