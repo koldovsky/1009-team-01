036a07523f4e008cf41a6f65ba878ddcd74abf7c
docia <matiushenko107@gmail.com>
2023-05-18 21:40:00 +0300

    added a go-to-top btn in js

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -1,9 +1,10 @@
 window.onload = () => {
-	let topBtn = document.getElementById("topBtn");
-
 	window.onscroll = function () {
 		scrollFunction();
 	};
+
+	// Create scroll to up button
+	const topBtn = createGoUpButton();
 
 	function scrollFunction() {
 		if (
@@ -20,4 +21,28 @@ window.onload = () => {
 function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
+}
+
+function createGoUpButton() {
+	// Create new elements
+	var button = document.createElement("button");
+	var img = document.createElement("img");
+
+	// Set the attributes for the button
+    button.onclick = topFunction;
+	button.setAttribute("id", "topBtn");
+	button.setAttribute("title", "Go to top");
+
+	// Set the attributes for the img
+	img.setAttribute("src", "images/new-bouquets/arrow-up-circle.svg");
+	img.setAttribute("alt", "Arrow to go to top");
+	img.setAttribute("class", "go-to-top-btn");
+
+	// Append img to the button
+	button.appendChild(img);
+
+	// Append the button to the body
+	document.body.appendChild(button);
+
+	return button;
 }
