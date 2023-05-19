a0af951eb13fa76ff5fd9c8f27a8eddd3cb33359
Dmytro Ohanov <102299301+dophotography@users.noreply.github.com>
2023-05-19 14:52:40 +0100

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -1,19 +1,25 @@
 // ---------- 1 ------------
 // Messi Goals              https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 //
-
-
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
 
 // ---------- 2 ------------
 // Make negative            https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 //
 
+const makeNegative = num => num >= 0 ? -num : num;
 
 
 // ---------- 3 ------------
 // Game Move               https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 //
 
+const move = (position, roll) => {
+    const newPosition = position + roll * 2;
+    return newPosition;
+}
 
 
 // ---------- 4 ------------
