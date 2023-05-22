2bb372c932d49d76778a6fcccf3079253e541577
Olenka Hryk <olena.hryk@gmail.com>
2023-05-22 13:17:46 +0300

    Create tasks-02.js

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,17 @@
+// -------------------------------------------------- 1 ----------------------------------------------------------------
+// Convert a string to an array:             https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+
+
+// -------------------------------------------------- 2 ----------------------------------------------------------------
+// DNA to RNA Conversion:                           https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+
+
+// -------------------------------------------------- 3 ----------------------------------------------------------------
+// Find Maximum and Minimum Values of a List     https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+
+
+// -------------------------------------------------- 4 ----------------------------------------------------------------
+// Smallest value of an array:                   https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
