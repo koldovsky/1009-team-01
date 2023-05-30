85ebe0de4c153142835abacb82ed0b0ad86cae58
docia <matiushenko107@gmail.com>
2023-05-30 07:37:52 +0300

    removed comments from go-to-top.js

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -2,7 +2,6 @@ window.addEventListener("load", () => {
 	window.addEventListener("scroll", scrollFunction);
 });
 
-// Create scroll to up button
 const topBtn = createGoUpButton();
 
 function scrollFunction() {
@@ -19,24 +18,19 @@ function topFunction() {
 }
 
 function createGoUpButton() {
-	// Create new elements
 	const button = document.createElement("button");
 	const img = document.createElement("img");
 
-	// Set the attributes for the button
 	button.addEventListener ("click", topFunction);
 	button.setAttribute("id", "topBtn");
 	button.setAttribute("title", "Go to top");
 
-	// Set the attributes for the img
 	img.setAttribute("src", "images/new-bouquets/arrow-up-circle.svg");
 	img.setAttribute("alt", "Arrow to go to top");
 	img.setAttribute("class", "go-to-top-btn");
 
-	// Append img to the button
 	button.appendChild(img);
 
-	// Append the button to the body
 	document.body.appendChild(button);
 
 	return button;
